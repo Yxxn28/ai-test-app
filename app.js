af2ddc63de60c7f2a6bc3f71f89e41bd9adaef5a
Yxxn28 <cdpzrvczhm@privaterelay.appleid.com>
2026-09-02 11:54:47 +0900

    Remove plaintext source after migration

--- app.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('#testButton');
-const result = document.querySelector('#result');
-
-button.addEventListener('click', () => {
-  result.textContent = '正常に動作しています。';
-});
